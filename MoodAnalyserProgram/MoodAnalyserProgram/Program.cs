4a6c429b97fac5044b95482df61eb12aa84d444b
faruqesay2 <faruqesay786@gmail.com>
2022-11-23 19:25:34 +0530

    Mood_Analyzer_Program

---
+++ MoodAnalyserProgram/MoodAnalyserProgram/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyzerProgram
+{
+    public class MoodAnalyser
+    {
+        public string message;
+        public MoodAnalyser()
+        {
+        }
+        public MoodAnalyser(string message)
+        {
+            this.message = message;
+        }
+        public string AnalyseMood()
+        {
+            try
+            {
+                if (this.message.Contains("Sad"))
+                {
+                    return "SAD";
+                }
+                else
+                {
+                    return "HAPPY";
+                }
+            }
+            catch (NullReferenceException ex)
+            {
+                return "HAPPY";
+            }
+        }
+    }
+}
