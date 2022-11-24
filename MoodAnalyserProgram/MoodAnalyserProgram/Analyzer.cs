7dc1ceca8dad650a164a4a3cd77b7361a4eb431a
FARUQESAY <113195467+faruqesay2@users.noreply.github.com>
2022-11-24 19:30:07 +0530

    UC_3

---
+++ MoodAnalyserProgram/MoodAnalyserProgram/Analyzer.cs
@@ -0,0 +1,44 @@
+[TestClass]
+    public class UnitTest1
+    {
+        //UC4
+        //TC 4.1  Given MoodAnalyse Class Name Should Return MoodAnalyser Object.
+        [TestMethod]
+        public void GivenMoodAnalyseClassName_ShouldReturnMoodAnalyseObject()
+        {
+            string message = null;
+            object expected = new MoodAnalyser(message);
+            object obj = MoodAnalyseFactory.CreateMoodAnalyse("MoodAnalyzerProblem.MoodAnalyser", "MoodAnalyser");
+            expected.Equals(obj);
+        }
+        //TC 4.2  Given Improper Class Name Should throw MoodAnalyssiException.
+        [TestMethod]
+        public void GivenImproperClassNameShouldThrowMoodAnalysisException()
+        {
+            string expected = "Class Not Found";
+            try
+            {
+                object moodAnalyseObject = MoodAnalyseFactory.CreateMoodAnalyse("MoodAnalyzerProblem.MoodAnalyser", "MoodAnalyser");
+
+            }
+            catch (MoodAnalyserException exception)
+            {
+                Assert.AreEqual(expected, exception.Message);
+            }
+        }
+        //TC 4.3  Given Improper Constructor should throw MoodAnalysisException
+        [TestMethod]
+        public void GivenImproperConstructorShouldThrowMoodAnalysisException()
+        {
+
+            string expected = "Constructor is Not Found";
+            try
+            {
+                object moodAnalyseObject = MoodAnalyseFactory.CreateMoodAnalyse("MoodAnalyzerProblem.MoodAnalyser", "MoodAnalyser");
+            }
+            catch (MoodAnalyserException exception)
+            {
+                Assert.AreEqual(expected, exception.Message);
+            }
+        }
+    }
